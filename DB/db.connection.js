902a4c50783b2cb68b21d8d2facc1593bf7e8668
mario malak <93185168+mariomalak1@users.noreply.github.com>
2024-09-21 22:10:13 +0300

    finish

--- DB/db.connection.js
+++ DB/db.connection.js
@@ -1,1 +1,17 @@
- 
+import {createPool} from "mysql2/promise";
+
+export const db = await createPool(
+    {
+        host: "localhost",
+        user: "root",
+        password: "mario@mysql123",
+        database: 'gym_app_pure_sql',
+    }
+);
+
+
+db.query("SELECT 1")
+    .then(() => {
+        console.log("connection done");
+    })
+    .catch(err => {console.error(err)});
--- index.js
+++ index.js
@@ -1,1 +1,33 @@
- 
+import Express from "express";
+import { router as apiRouter } from "./Src/Routers/index.routers.js";
+
+const app = Express();
+
+// Middleware to parse incoming JSON requests
+app.use(Express.json());
+
+// Define your routes
+app.use("/api", apiRouter);
+
+// 404 Error Handling Middleware for invalid paths
+app.use((req, res, next) => {
+    res.status(404).json({
+        error: 'Not Found',
+        message: `The path ${req.originalUrl} does not exist`
+    });
+});
+
+// General Error Handling Middleware for any other server errors
+app.use((err, req, res, _) => {
+    console.error(err.stack); // Log the error stack for debugging
+    res.status(500).json({
+        error: 'Internal Server Error',
+        message: err.message
+    });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+});
